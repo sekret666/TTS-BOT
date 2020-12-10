d59a8cc473ad634b8e2d9369c5ebea7e1481ab6a
Rithunand <73537720+Benchamxd@users.noreply.github.com>
2020-12-10 17:29:30 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -38,12 +38,10 @@ $text = $message->text;
 if($text == '/start'){
 bot('sendmessage',[
  'chat_id'=>$chat_id,
- 'text'=><<<EOM
+ 'text'=>"$START_MESSAGE
 
-$START_MESSAGE
-
-YOU CAN ALSO MAKE A BOT LIKE THIS FROM <a href="https://github.com/BenchamXD/TTS-BOT">HERE</a>
-EOM;,
+YOU CAN ALSO MAKE A BOT LIKE THIS FROM <a href="https://github.com/BenchamXD/TTS-BOT">HERE</a>",
+'parse_mode'=>"HTML",
 'reply_to_message_id'=>$message_id,
  ]);
 }
